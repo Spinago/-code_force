532d08558d8cc792b17a39ed19747c4e255be7ec
Thomas <thomas@thomasspina.com>
2020-05-31 18:20:22 -0600

    just doing easy problems to get back into it with cpp

---
+++ WayTooLongWords.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX_LENGTH 10
+
+int main(int argc, const char * argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int i;
+    cin >> i;
+    
+    while (i > 0) {
+        string str;
+        cin >> str;
+        
+        if (str.length() <= MAX_LENGTH) {
+            cout << str << "\n";
+        } else {
+            cout << str.at(0) << str.length() - 2 << str.at(str.length() - 1) << "\n";
+        }
+        
+        i--;
+    }
+    
+    return 0;
+}

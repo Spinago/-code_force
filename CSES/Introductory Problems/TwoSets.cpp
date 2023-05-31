4af8a38c3fe4103824ec076823ba71b270173669
Thomas <thomas@thanos.local>
2023-05-30 21:55:07 -0400

    Completed two sets using dynamic programming. Using two arrays and the sum that you're supposed to have in the array, it's easy to reach the response

--- CSES/Introductory Problems/TwoSets.cpp
+++ CSES/Introductory Problems/TwoSets.cpp
@@ -1,10 +1,45 @@
 #include <bits/stdc++.h>
 
+#define ll long long
+
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
+
+    ll n; cin >> n;
+
+    ll sum = n * (n + 1) / 2;
+
+    if (sum % 2 != 0) {
+        cout << "NO";
+        return 0;
+    }
+
+    ll curr_sum = sum / 2;
+
+    vector<ll> a, b;
+
+    for (ll i = n; i >= 1; i--) {
+        if (i <= curr_sum) {
+            
+            a.push_back(i);
+            curr_sum -= i;
+        } else {
+            b.push_back(i);
+        }
+    }
+    cout << "YES\n" << a.size() << "\n";
+    for (ll i : a) {
+        cout << i << " ";
+    }
+
+    cout << "\n" << b.size() << "\n";
+    for (ll i : b) {
+        cout << i << " ";
+    }
+
 
     return 0;
 }
